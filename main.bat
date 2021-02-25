bc70d55b7d3d030b3e0a31084cd2dc54822d0c9d
WilcoTerink <terinkw@gmail.com>
2021-02-26 08:49:37 +1300

    Build in loop in batch file that if python script stops for whatever reason it starts it again.

--- main.bat
+++ main.bat
@@ -1,6 +1,8 @@
 echo started %date% %time% >> %~dp0\backup.log
 call D:\Miniconda3\Scripts\activate.bat D:\Miniconda3 >> %~dp0\backup.log 2>&1
 call activate dhi_hydrotel >> %~dp0\backup.log 2>&1
+:start
 call python %~dp0\dhi_to_hydrotel.py >> %~dp0\backup.log 2>&1
+goto start
 call python %~dp0\send_email.py >> %~dp0\backup.log 2>&1
 echo ended %date% %time% >> %~dp0\backup.log
